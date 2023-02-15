d8bba39de866e998470fe70c8afc80ebaeb41e01
tate <tate@ens.domains>
2023-02-15 14:23:43 +1100

    init

---
+++ src/App.tsx
@@ -0,0 +1,44 @@
+import "./App.css";
+import DAOLogo from "./assets/DAOLogo.svg";
+import MainItems from "./components/MainItems";
+
+const headerLinks = [
+  {
+    text: "Basics",
+    link: "https://basics.ensdao.org",
+  },
+  {
+    text: "Grants",
+    link: "https://ensgrants.xyz",
+  },
+  {
+    text: "Agora",
+    link: "https://agora.ensdao.org",
+  },
+  {
+    text: "Forum",
+    link: "https://discuss.ens.domains/",
+  },
+  {
+    text: "Docs",
+    link: "https://docs.ens.domains/v/governance/",
+  },
+];
+
+function App() {
+  return (
+    <div className="App">
+      <div className="header">
+        <img src={DAOLogo} />
+        <div className="header-items">
+          {headerLinks.map((link) => (
+            <a href={link.link}>{link.text}</a>
+          ))}
+        </div>
+      </div>
+      <MainItems />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/MainItems.tsx
@@ -0,0 +1,122 @@
+import dao from "../assets/dao.svg";
+import discord from "../assets/discord.svg";
+import discuss from "../assets/discuss.svg";
+import governance from "../assets/governance.svg";
+import mirror from "../assets/mirror.svg";
+import tally from "../assets/tally.svg";
+
+type BoxData = {
+  icon: string;
+  text: string;
+  color: string;
+  backgroundColor: string;
+  borderColor: string;
+  link: string;
+};
+
+const standardBoxData: BoxData[] = [
+  {
+    icon: dao,
+    text: "Delegate $ENS",
+    color: "#A764FC",
+    backgroundColor: "rgba(180, 120, 255, 0.1)",
+    borderColor: "#D6B6FF",
+    link: "https://delegate.ens.domains",
+  },
+  {
+    icon: discuss,
+    text: "Join the forum",
+    color: "#DE7224",
+    backgroundColor: "rgba(238, 148, 83, 0.1)",
+    borderColor: "#F8D1B6",
+    link: "https://discuss.ens.domains/",
+  },
+  {
+    icon: tally,
+    text: "Vote on proposals",
+    color: "#00CBB5",
+    backgroundColor: "rgba(98, 244, 228, 0.2)",
+    borderColor: "#62F4E4",
+    link: "https://www.tally.xyz/governance/eip155:1:0x323A76393544d5ecca80cd6ef2A560C6a395b7E3",
+  },
+  {
+    icon: governance,
+    text: "Documentation",
+    color: "#4D90F1",
+    backgroundColor: "rgba(77, 144, 241, 0.1)",
+    borderColor: "#B0D0FF",
+    link: "https://docs.ens.domains/v/governance/",
+  },
+  {
+    icon: discord,
+    text: "Chat on Discord",
+    color: "#854BFF",
+    backgroundColor: "#E1DAF7",
+    borderColor: "#C0B2EA",
+    link: "https://chat.ens.domains/",
+  },
+  {
+    icon: mirror,
+    text: "Read on Mirror",
+    color: "#2C2C32",
+    backgroundColor: "#E8E7EF",
+    borderColor: "#D2CFE4",
+    link: "https://ens.mirror.xyz/",
+  },
+];
+
+const StandardBox = ({
+  color,
+  backgroundColor,
+  text,
+  icon,
+  borderColor,
+  link,
+}: BoxData) => {
+  return (
+    <a
+      className="box"
+      style={
+        {
+          "--background": backgroundColor,
+          "--border": borderColor,
+        } as React.CSSProperties
+      }
+      href={link}
+      target={"_blank"}
+    >
+      <div>
+        <div>
+          <img
+            style={{ width: icon === governance ? "40px" : "initial" }}
+            src={icon}
+          />
+        </div>
+        <div>
+          <div
+            className="bottom-text"
+            style={{ "--text": color } as React.CSSProperties}
+          >
+            {text}
+          </div>
+        </div>
+      </div>
+    </a>
+  );
+};
+
+function MainItems() {
+  return (
+    <main>
+      <div className="content">
+        <div className="content-grid">
+          {standardBoxData.map((boxData) => (
+            <StandardBox key={boxData.text} {...boxData} />
+          ))}
+        </div>
+      </div>
+    </main>
+  );
+}
+
+export default MainItems;
